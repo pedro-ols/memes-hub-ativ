6d29c8f9e16d5161ff1e0278840b77b6ae1103bc
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2025-04-03 14:46:50 -0300

    feat: create news letter section component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,6 +6,7 @@ import Feed from "./components/feedSection";
 import Footer from "./components/footer";
 import CreatorsSection from "./components/creatorsSection";
 import FeaturedMemes from "./components/featuredSection";
+import NewsLetterSection from "./components/newsLetterSection";
 
 export default function Home() {
 
@@ -49,32 +50,7 @@ export default function Home() {
           {/* FIM COMPONENTE: CreatorsSection */}
 
           {/* COMPONENTE: NewsletterSection */}
-          <section className={styles.newsletterSection}>
-            <div className={styles.newsletterContent}>
-              <h2 className={styles.newsletterTitle}>Fique por dentro!</h2>
-              <p className={styles.newsletterDescription}>
-                Assine nossa newsletter e receba os melhores memes toda semana!
-              </p>
-              <div className={styles.newsletterForm}>
-                <input
-                  type="email"
-                  placeholder="Seu melhor e-mail"
-                  className={styles.newsletterInput}
-                />
-                <button className={styles.newsletterButton}>Assinar</button>
-              </div>
-              <p className={styles.newsletterDisclaimer}>
-                Nós respeitamos sua privacidade. Cancele quando quiser.
-              </p>
-            </div>
-            <div className={styles.newsletterImageContainer}>
-              <img
-                src="https://i.imgur.com/OELrGl5.jpg"
-                alt="Newsletter"
-                className={styles.newsletterImage}
-              />
-            </div>
-          </section>
+          <NewsLetterSection/>
           {/* FIM COMPONENTE: NewsletterSection */}
         </div>
 
