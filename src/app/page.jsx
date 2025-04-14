80958747c8fbb7e218258e7d6ab5fa9ab2d39734
Pedro de Oliveira Santos <pedrosantos.tds2.senai@gmail.com>
2025-04-14 18:33:28 -0300

    feat: create header component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
-import styles from "./memes.module.css";
+import styles from "./page.module.css";
+import Header from "./components/header"
 
 export default function Home() {
   // Array de dados dos memes para serem passados como props
@@ -167,28 +168,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       {/* COMPONENTE: Header */}
-      <header className={styles.header}>
-        <div className={styles.logo}>
-          <h1>MemeVerse</h1>
-          <span>🌟</span>
-        </div>
-        <nav className={styles.nav}>
-          <a href="#" className={styles.active}>
-            Home
-          </a>
-          <a href="#">Trending</a>
-          <a href="#">Create</a>
-          <a href="#">Categories</a>
-          <a href="#">Profile</a>
-        </nav>
-
-        <div className={styles.userActions}>
-          <button className={styles.notificationBtn}>🔔</button>
-          <button className={styles.userAvatar}>
-            <img src="https://i.pravatar.cc/150?img=3" alt="Your profile" />
-          </button>
-        </div>
-      </header>
+      <Header />
+      
       {/* FIM COMPONENTE: Header */}
 
       <div className={styles.mainContent}>
